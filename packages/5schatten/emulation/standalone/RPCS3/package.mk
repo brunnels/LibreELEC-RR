c9b2520522a19daa1c11c2be6d3ee4e5611ae686
5schatten <supervisedthinking@gmail.com>
2019-10-15 11:17:55 +0200

    RPCS3: updated to git 1a9e06d

--- packages/5schatten/emulation/standalone/RPCS3/package.mk
+++ packages/5schatten/emulation/standalone/RPCS3/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="RPCS3"
-PKG_VERSION="2171ffdab2f2af789311b2264fa9e8183dca8e2a" # v0.0.7+
+PKG_VERSION="1a9e06d3c66621f66bccf05962b6d5c29587c206" # v0.0.7+
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://rpcs3.net"
